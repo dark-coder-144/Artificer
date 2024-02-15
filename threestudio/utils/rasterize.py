d8c289352b43e37cb4e05362887b071ab4128fd5
K SHRAVAN <shravan2002k@gmail.com>
2024-02-15 16:09:11 +0530

    Update rasterize.py

--- threestudio/utils/rasterize.py
+++ threestudio/utils/rasterize.py
@@ -11,10 +11,10 @@ class NVDiffRasterizerContext:
 
     def initialize_context(
         self, context_type: str, device: torch.device
-    ) -> Union[dr.RasterizeGLContext, dr.RasterizeCudaContext]:
-        if context_type == "gl":
-            return dr.RasterizeGLContext(device=device)
-        elif context_type == "cuda":
+    ) -> Union[dr.RasterizeCudaContext]:
+        # if context_type == "gl":
+        #     return dr.RasterizeGLContext(device=device)
+        if context_type == "cuda":
             return dr.RasterizeCudaContext(device=device)
         else:
             raise ValueError(f"Unknown rasterizer context type: {context_type}")
